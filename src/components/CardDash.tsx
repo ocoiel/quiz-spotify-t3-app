fbccb9cb9dd1158a4f3f51a83ab416ac59ca66fa
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2022-12-21 00:39:46 -0300

    feat: dropdown in card

--- src/components/CardDash.tsx
+++ src/components/CardDash.tsx
@@ -30,66 +30,115 @@ export default function CardDash({
   };
 
   return (
-    <div
-      className="delay-50 group min-h-max w-[26rem] rounded-2xl bg-zinc-900 p-4 duration-150 hover:-translate-y-2 hover:bg-zinc-700 hover:shadow-md "
-      // href={`${link}`}
-    >
-      <Image
-        src={`/${image}.svg`}
-        className="w-full rounded-xl shadow"
-        width={228}
-        height={228}
-        alt={`${title}-${description}`}
-      />
-      <h3 className="mt-5 text-2xl font-bold text-gray-200">{title}</h3>
-      <p className="mt-2 text-sm font-light text-gray-400"> {description}</p>
-      {modal ? (
-        <div>
-          <Dropdown
-            title="Options"
-            className="bg-transparent text-gray-300 hover:text-white"
-            icon={<BiSlider size={17} />}
-            href={""}
-          >
-            <DropdownItem icon={<BiCopy size={17} />} href={""}>
-              Copy
-            </DropdownItem>
-            <DropdownItem
-              icon={<BiEdit size={17} />}
-              onClick={handleEditModal}
-              href={""}
-            >
-              Edit
-            </DropdownItem>
-            <DropdownItem
-              icon={<BiTrash size={17} />}
-              onClick={handleDeleteModal}
-              href={""}
-            >
-              Delete
-            </DropdownItem>
-          </Dropdown>
-          {/* <Modal
-            title={`Edit: /s/${props.slug}`}
-            open={editModal}
-            close={handleEditModal}
-          >
-            <Edit
-              id={props.id}
-              slug={props.slug}
-              url={props.url}
-              description={props.description}
-            />
-          </Modal>
-          <Modal
-            title={`Delete: /s/${props.slug}`}
-            open={deleteModal}
-            close={handleDeleteModal}
-          >
-            <Delete id={props.id} />
-          </Modal> */}
-        </div>
-      ) : null}
-    </div>
+    // changing tag <a> -> <div> because of hydrations issues
+
+    // <div
+    //   className="delay-50 group min-h-max w-[26rem] rounded-2xl bg-zinc-900 p-4 duration-150 hover:-translate-y-2 hover:bg-zinc-700 hover:shadow-md "
+    //   // href={`${link}`}
+    //   // onClick={() => alert("amor")}
+    // >
+    //   <Image
+    //     src={`/${image}.svg`}
+    //     className="w-full rounded-xl shadow"
+    //     width={228}
+    //     height={228}
+    //     alt={`${title}-${description}`}
+    //   />
+    //   <h3 className="mt-5 text-2xl font-bold text-gray-200">{title}</h3>
+    //   <p className="mt-2 text-sm font-light text-gray-400"> {description}</p>
+
+    // </div>
+    modal ? (
+      <Dropdown
+        title={title}
+        description={description}
+        className="bg-transparent text-gray-900 hover:text-gray-700"
+        icon={<BiSlider size={18} />}
+        href={""}
+      >
+        <DropdownItem icon={<BiCopy size={17} />} href={""}>
+          Copy
+        </DropdownItem>
+        <DropdownItem
+          icon={<BiEdit size={18} />}
+          // onClick={handleEditModal}
+          onClick={() => alert("amor")}
+          href={""}
+        >
+          botao tremendamente grande q louco
+        </DropdownItem>
+        <DropdownItem
+          icon={<BiTrash size={18} />}
+          onClick={handleDeleteModal}
+          href={""}
+        >
+          Delete
+        </DropdownItem>
+      </Dropdown>
+    ) : (
+      <div
+        className="delay-50 group min-h-max w-[26rem] rounded-2xl bg-zinc-900 p-4 duration-150 hover:-translate-y-2 hover:bg-zinc-700 hover:shadow-md "
+        // href={`${link}`}
+        // onClick={() => alert("amor")}
+      >
+        <Image
+          src={`/${image}.svg`}
+          className="w-full rounded-xl shadow"
+          width={228}
+          height={228}
+          alt={`${title}-${description}`}
+        />
+        <h3 className="mt-5 text-2xl font-bold text-gray-200">{title}</h3>
+        <p className="mt-2 text-sm font-light text-gray-400"> {description}</p>
+      </div>
+    )
   );
 }
+
+// {modal ? (
+//   <div>
+//     <Dropdown
+//       title="Options"
+//       className="bg-transparent text-gray-900 hover:text-gray-700"
+//       icon={<BiSlider size={17} />}
+//       href={""}
+//     >
+//       <DropdownItem icon={<BiCopy size={17} />} href={""}>
+//         Copy
+//       </DropdownItem>
+//       <DropdownItem
+//         icon={<BiEdit size={17} />}
+//         onClick={handleEditModal}
+//         href={""}
+//       >
+//         Edit
+//       </DropdownItem>
+//       <DropdownItem
+//         icon={<BiTrash size={17} />}
+//         onClick={handleDeleteModal}
+//         href={""}
+//       >
+//         Delete
+//       </DropdownItem>
+//     </Dropdown>
+//     <Modal
+//       title={`Edit: /s/${props.slug}`}
+//       open={editModal}
+//       close={handleEditModal}
+//     >
+//       <Edit
+//         id={props.id}
+//         slug={props.slug}
+//         url={props.url}
+//         description={props.description}
+//       />
+//     </Modal>
+//     <Modal
+//       title={`Delete: /s/${props.slug}`}
+//       open={deleteModal}
+//       close={handleDeleteModal}
+//     >
+//       <Delete id={props.id} />
+//     </Modal>
+//   </div>
+// ) : null}
